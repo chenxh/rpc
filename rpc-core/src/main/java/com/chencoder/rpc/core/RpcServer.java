9fa6eda8084f3939d9faaaa6c02873169cb54070
chenxh <807251894@qq.com>
2017-07-10 22:21:34 +0800

    add rpcServer

--- rpc-core/src/main/java/com/chencoder/rpc/core/RpcServer.java
+++ rpc-core/src/main/java/com/chencoder/rpc/core/RpcServer.java
@@ -1,5 +1,39 @@
 package com.chencoder.rpc.core;
 
+import com.chencoder.rpc.common.config.RegistryConfig;
+import com.chencoder.rpc.common.config.ServerConfig;
+import com.chencoder.rpc.core.provider.Exporter;
+
 public class RpcServer {
+	
+	private ServerConfig config;
+	
+	private Exporter exporter = null;
+
+	public RpcServer(ServerConfig config){
+		setConfig(config);
+	}
+	
+	public RpcServer(ServerConfig config, RegistryConfig registryConfig){
+		setConfig(config);
+		exporter = new Exporter(registryConfig);
+	}
+	
+	public <T> void exportService(Class<T> serviceClass, T implObject){
+		if(exporter != null)
+			exporter.exportService(serviceClass, implObject, config.getPort());
+	}
+	
+	public void startServer(){
+		
+	}
+
+	public ServerConfig getConfig() {
+		return config;
+	}
+
+	public void setConfig(ServerConfig config) {
+		this.config = config;
+	}
 
 }
